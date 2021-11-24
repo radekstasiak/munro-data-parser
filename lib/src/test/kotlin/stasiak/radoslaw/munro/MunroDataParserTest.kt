b3d62b1fb2c0b1cc570a96dd529315aeb0ff7891
radekstasiak <radek.stasiak@gmail.com>
2021-11-24 23:47:57 +0000

    update tests

--- lib/src/test/kotlin/stasiak/radoslaw/munro/MunroDataParserTest.kt
+++ lib/src/test/kotlin/stasiak/radoslaw/munro/MunroDataParserTest.kt
@@ -20,7 +20,7 @@ class MunroDataParserTest {
 
     @Test
     fun `parser maps results correctly when no Query provided`() {
-        val parser = MunroDataParser(testDataFileInputStream,)
+        val parser = MunroDataParser(testDataFileInputStream)
         assertEquals(509, parser.getResults().size)
     }
 
@@ -458,6 +458,202 @@ class MunroDataParserTest {
         assertEquals("NG440224", munroDataResults[7].gridRef)
     }
 
+
+    @Test
+    fun `parser returns data not sorted`() {
+        val parser = MunroDataParser(testDataFileInputStream)
+        val munroDataQuery = MunroDataQuery.Builder()
+            .setMinHeightInMeters(922.0)
+            .setMaxHeightInMeters(924.0)
+            .build()
+
+        val munroDataResults = parser.getResults(munroDataQuery)
+
+        assertEquals(11, munroDataResults.size)
+
+        assertEquals("Carn Gorm - An Sgorr", munroDataResults[0].name)
+        assertEquals(924.0, munroDataResults[0].heightInMeters)
+
+        assertEquals("Beinn Mhanach - Beinn a' Chuirn", munroDataResults[1].name)
+        assertEquals(923.0, munroDataResults[1].heightInMeters)
+
+        assertEquals("Beinn Eibhinn - Meall Glas Choire", munroDataResults[2].name)
+        assertEquals(924.0, munroDataResults[2].heightInMeters)
+
+        assertEquals("Beinn Eibhinn - Mullach Coire nan Nead", munroDataResults[3].name)
+        assertEquals(922.0, munroDataResults[3].heightInMeters)
+
+        assertEquals("Creag Pitridh", munroDataResults[4].name)
+        assertEquals(924.0, munroDataResults[4].heightInMeters)
+
+        assertEquals("Glas Maol - Meall Odhar", munroDataResults[5].name)
+        assertEquals(922.0, munroDataResults[5].heightInMeters)
+
+        assertEquals("Carn Dearg SE Top", munroDataResults[6].name)
+        assertEquals(923.6, munroDataResults[6].heightInMeters)
+
+        assertEquals("Beinn Alligin - Tom na Gruagaich", munroDataResults[7].name)
+        assertEquals(922.0, munroDataResults[7].heightInMeters)
+
+        assertEquals("Sgurr nan Each", munroDataResults[8].name)
+        assertEquals(923.0, munroDataResults[8].heightInMeters)
+
+        assertEquals("Meall Gorm SE Top", munroDataResults[9].name)
+        assertEquals(922.0, munroDataResults[9].heightInMeters)
+
+        assertEquals("An Coileachan", munroDataResults[10].name)
+        assertEquals(923.9, munroDataResults[10].heightInMeters)
+
+    }
+
+    @Test
+    fun `parser returns data filtered and sorted by height in descending order`() {
+        val parser = MunroDataParser(testDataFileInputStream)
+        val munroDataQuery = MunroDataQuery.Builder()
+            .sortByHeightInMeters(false)
+            .setMinHeightInMeters(922.0)
+            .setMaxHeightInMeters(924.0)
+            .build()
+        val munroDataResults = parser.getResults(munroDataQuery)
+
+        assertEquals(11, munroDataResults.size)
+
+        assertEquals("Carn Gorm - An Sgorr", munroDataResults[0].name)
+        assertEquals(924.0, munroDataResults[0].heightInMeters)
+
+        assertEquals("Beinn Eibhinn - Meall Glas Choire", munroDataResults[1].name)
+        assertEquals(924.0, munroDataResults[1].heightInMeters)
+
+        assertEquals("Creag Pitridh", munroDataResults[2].name)
+        assertEquals(924.0, munroDataResults[2].heightInMeters)
+
+        assertEquals("An Coileachan", munroDataResults[3].name)
+        assertEquals(923.9, munroDataResults[3].heightInMeters)
+
+        assertEquals("Carn Dearg SE Top", munroDataResults[4].name)
+        assertEquals(923.6, munroDataResults[4].heightInMeters)
+
+        assertEquals("Beinn Mhanach - Beinn a' Chuirn", munroDataResults[5].name)
+        assertEquals(923.0, munroDataResults[5].heightInMeters)
+
+        assertEquals("Sgurr nan Each", munroDataResults[6].name)
+        assertEquals(923.0, munroDataResults[6].heightInMeters)
+
+        assertEquals("Beinn Eibhinn - Mullach Coire nan Nead", munroDataResults[7].name)
+        assertEquals(922.0, munroDataResults[7].heightInMeters)
+
+        assertEquals("Glas Maol - Meall Odhar", munroDataResults[8].name)
+        assertEquals(922.0, munroDataResults[8].heightInMeters)
+
+        assertEquals("Beinn Alligin - Tom na Gruagaich", munroDataResults[9].name)
+        assertEquals(922.0, munroDataResults[9].heightInMeters)
+
+        assertEquals("Meall Gorm SE Top", munroDataResults[10].name)
+        assertEquals(922.0, munroDataResults[10].heightInMeters)
+
+
+    }
+
+    @Test
+    fun `parser returns data sorted first descending by height and then ascending by name`() {
+        val parser = MunroDataParser(testDataFileInputStream)
+        val munroDataQuery = MunroDataQuery.Builder()
+            .setMinHeightInMeters(922.0)
+            .setMaxHeightInMeters(924.0)
+            .sortByHeightInMeters(false)
+            .sortAlphabeticallyByName(true)
+            .build()
+
+        val munroDataResults = parser.getResults(munroDataQuery)
+
+        assertEquals(11, munroDataResults.size)
+
+        assertEquals("Beinn Eibhinn - Meall Glas Choire", munroDataResults[0].name)
+        assertEquals(924.0, munroDataResults[0].heightInMeters)
+
+        assertEquals("Carn Gorm - An Sgorr", munroDataResults[1].name)
+        assertEquals(924.0, munroDataResults[1].heightInMeters)
+
+        assertEquals("Creag Pitridh", munroDataResults[2].name)
+        assertEquals(924.0, munroDataResults[2].heightInMeters)
+
+        assertEquals("An Coileachan", munroDataResults[3].name)
+        assertEquals(923.9, munroDataResults[3].heightInMeters)
+
+        assertEquals("Carn Dearg SE Top", munroDataResults[4].name)
+        assertEquals(923.6, munroDataResults[4].heightInMeters)
+
+        assertEquals("Beinn Mhanach - Beinn a' Chuirn", munroDataResults[5].name)
+        assertEquals(923.0, munroDataResults[5].heightInMeters)
+
+        assertEquals("Sgurr nan Each", munroDataResults[6].name)
+        assertEquals(923.0, munroDataResults[6].heightInMeters)
+
+        assertEquals("Beinn Alligin - Tom na Gruagaich", munroDataResults[7].name)
+        assertEquals(922.0, munroDataResults[7].heightInMeters)
+
+        assertEquals("Beinn Eibhinn - Mullach Coire nan Nead", munroDataResults[8].name)
+        assertEquals(922.0, munroDataResults[8].heightInMeters)
+
+        assertEquals("Glas Maol - Meall Odhar", munroDataResults[9].name)
+        assertEquals(922.0, munroDataResults[9].heightInMeters)
+
+        assertEquals("Meall Gorm SE Top", munroDataResults[10].name)
+        assertEquals(922.0, munroDataResults[10].heightInMeters)
+
+    }
+
+    @Test
+    fun `parser returns data sorted first descending by name and then ascending by height`() {
+        val resource: URL = MunroDataParserTest::class.java.getResource("/munrotab_duplicate_names.csv")!!
+        val testDataFileInputStream = FileInputStream(Paths.get(resource.toURI()).toFile())
+        val parser = MunroDataParser(testDataFileInputStream)
+        val munroDataQuery = MunroDataQuery.Builder()
+            .setMinHeightInMeters(922.0)
+            .setMaxHeightInMeters(924.0)
+            .sortAlphabeticallyByName(false)
+            .sortByHeightInMeters(true)
+            .build()
+
+        val munroDataResults = parser.getResults(munroDataQuery)
+
+        assertEquals(11, munroDataResults.size)
+
+        assertEquals("Sgurr nan Each", munroDataResults[0].name)
+        assertEquals(923.0, munroDataResults[0].heightInMeters)
+
+        assertEquals("Meall Gorm SE Top", munroDataResults[1].name)
+        assertEquals(922.0, munroDataResults[1].heightInMeters)
+
+        assertEquals("Glas Maol - Meall Odhar", munroDataResults[2].name)
+        assertEquals(922.0, munroDataResults[2].heightInMeters)
+
+        assertEquals("Creag Pitridh", munroDataResults[3].name)
+        assertEquals(924.0, munroDataResults[3].heightInMeters)
+
+        assertEquals("Carn Gorm - An Sgorr", munroDataResults[4].name)
+        assertEquals(923.0, munroDataResults[4].heightInMeters)
+
+        assertEquals("Carn Dearg SE Top", munroDataResults[5].name)
+        assertEquals(923.6, munroDataResults[5].heightInMeters)
+
+        assertEquals("Carn Dearg SE Top", munroDataResults[6].name)
+        assertEquals(924.0, munroDataResults[6].heightInMeters)
+
+        assertEquals("Carn Dearg SE Top", munroDataResults[7].name)
+        assertEquals(923.6, munroDataResults[7].heightInMeters)
+
+
+        assertEquals("Beinn Eibhinn - Meall Glas Choire", munroDataResults[8].name)
+        assertEquals(924.0, munroDataResults[8].heightInMeters)
+
+        assertEquals("Beinn Alligin - Tom na Gruagaich", munroDataResults[9].name)
+        assertEquals(922.0, munroDataResults[9].heightInMeters)
+
+        assertEquals("An Coileachan", munroDataResults[10].name)
+        assertEquals(923.9, munroDataResults[10].heightInMeters)
+    }
+
     @Test
     fun `parser maps MunroDataRecord to MunroDataModel correctly when multiple filters and sorting rule are set`() {
         val parser = MunroDataParser(testDataFileInputStream)
@@ -488,7 +684,6 @@ class MunroDataParserTest {
         assertEquals("Munro", munroDataResults[2].hillCategory)
         assertEquals("NG946113", munroDataResults[2].gridRef)
     }
-
 
 
 }
